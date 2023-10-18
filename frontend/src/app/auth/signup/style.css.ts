7be8359a4d8f2a0299d3d36033adadc3e526b28f
yuto ichinose <yuto.0526.sano@gmail.com>
2023-10-19 00:13:56 +0900

    fix: 中央寄せになるように修正

--- frontend/src/app/auth/signup/style.css.ts
+++ frontend/src/app/auth/signup/style.css.ts
@@ -3,7 +3,7 @@ import { style } from "@vanilla-extract/css";
 const styles={
     containar:style({
         width:"75%",//長さはブラウザの横の75% 
-        marginTop:"150px auto",// 上に50pxで以外は中央よせに,
+        margin:"150px auto 0 auto",// 上に50pxで以外は中央よせに,
         textAlign:"center",//文字を中央に寄せる(デフォルトは中央添え)
     }),
 }
